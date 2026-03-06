5f33c2d10076198aae79993b981efdc43d210c15
Hubert Lee 🤖 <hube@users.noreply.github.com>
2026-03-06 15:26:43 -0800

    Add file hash verification to AWS CLI installation script

--- install-scripts/aws-cli.sh
+++ install-scripts/aws-cli.sh
@@ -3,8 +3,11 @@
 # Install the AWS CLI. Instructions originally copied from
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscli-exe-linux-aarch64.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip.sha256" -o "awscli-exe-linux-aarch64.zip.sha256"
+sha256sum -c awscli-exe-linux-aarch64.zip.sha256
+
+unzip awscli-exe-linux-aarch64.zip
 ./aws/install
 
 # Set default AWS region
