a7b7aaa68ba9fc1ff81a6430c2506a9930f73942
Hubert Lee 🤖 <hube@users.noreply.github.com>
2026-05-27 16:01:04 -0700

    Define Claude installation as a local feature

---
+++ .devcontainer/local-features/claude/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Install Claude Code. Installation location originally copied from
+# https://docs.anthropic.com/en/docs/claude-code/getting-started
+
+if [[ $EUID -ne $(id -u ${_CONTAINER_USER}) ]]
+then
+  echo ">Copying config to the remote user's home directory"
+
+  # rsync allows copying files over while changing ownership
+  rsync -r --chown=${_CONTAINER_USER}:${_CONTAINER_USER} \
+      config/. /home/${_CONTAINER_USER}
+
+  exec sudo -iu "${_CONTAINER_USER}" "$(realpath $0)"
+fi
+
+echo ">Installing Claude Code"
+
+curl -fsSL https://claude.ai/install.sh | bash
+
+echo ">Done installing Claude code"
--- install-scripts/copy_dotfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo ">Copying dotfiles to the remote user's home directory"
-
-cp -r home/. ~
-
-echo ">Done copying dotfiles"
