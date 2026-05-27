7343e89704085346aab98db31c8e63dbb53f2105
Hubert Lee 🤖 <hube@users.noreply.github.com>
2026-05-27 14:04:04 -0700

    Refactor to a minimal devcontainer

--- install-scripts/claude.sh
+++ install-scripts/claude.sh
@@ -3,4 +3,8 @@
 # Install Claude Code. Instructions originally copied from
 # https://docs.anthropic.com/en/docs/claude-code/getting-started
 
+echo ">Installing Claude Code"
+
 curl -fsSL https://claude.ai/install.sh | bash
+
+echo ">Done installing Claude code"
--- install-scripts/user/oh-my-zsh.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Install oh-my-zsh
-# https://github.com/ohmyzsh/ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
