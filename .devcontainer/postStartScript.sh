8198dfd5b41c4164362ffc6380f00e455a2e7995
Hubert Lee 🤖 <hube@users.noreply.github.com>
2026-06-02 12:43:05 -0700

    Copy known_hosts file from host to devcontainer

---
+++ .devcontainer/postStartScript.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# SSH configuration
+
+## Enable rw permissions on the SSH auth socket
+sudo chmod 666 /run/host-services/ssh-auth.sock
+
+## Copy known_hosts to reduce SSH prompts
+mkdir -p ~/.ssh
+cp ~/.ssh-host/known_hosts ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
